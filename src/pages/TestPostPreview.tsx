5eace32f1e5e2cd9e82325f37ed93259f7095fc6
Michael Rangel <michael@getclear.io>
2025-06-13 09:47:56 -0400

    🧹 Mark TestPostPreview.tsx as deprecated

---
+++ src/pages/TestPostPreview.tsx
@@ -0,0 +1,6 @@
+// DEPRECATED FILE - DELETED IN PREVIEW SYSTEM v3.0 CLEANUP
+// This empty file has been removed as part of the cleanup process
+
+export default function DeprecatedTestComponent() {
+  return null;
+}
